10c1289487ed1a2931e02df456e9e9f3ce8748d7
Paul Rieunier <61982963+PaulRieunier@users.noreply.github.com>
2020-04-27 00:03:08 +0200

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,7 @@
+On identifie les Individus selon ces critères : Identifiant, 2 Parents, Génération, Salaire.
+
+#type Individu = tuple[int,tuple[int,int],int,int]
+
+Def ListeSalaireCroissant(L) :
+	"""list[Individu] -> list[int]"""
+	
