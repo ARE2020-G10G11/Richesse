8539b53ec14a117f8a5a73e10a960b8db1982979
Paul Rieunier <61982963+PaulRieunier@users.noreply.github.com>
2020-04-27 02:03:48 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,9 +1,9 @@
 """On identifie les Individus selon ces critères : Identifiant, 2 Parents, Génération, Salaire."""
 
-#type Individu = tuple[int,tuple[int,int],int,Number]
+#type Individu = tuple[int,tuple[int,int],int,float]
 
 def MinEtPos(L) :
-	"""list[Number] -> list[Number]
+	"""list[Number] -> list[float]
 	Renvoie le Minimum et sa position """
 	for i in range(len(L)) :
 		if len([k for k in L if k <L[i]]) == 0 :
@@ -15,20 +15,25 @@ def RangementCroissant(L) :
 	if len(L) == 0 :
 		return []
 	else :
-		L1 = MinEtPos(L)[0]
+		L1 = MinEtPos(L)
 		return [L1[0]] + RangementCroissant(L[:L1[1]]+L[L1[1]+1:])
 
 def Moyenne(L) :
-	"""list[Number] -> Number
+	"""list[float] -> float
 	Renvoie la Moyenne de la liste"""
+	n = 0
+	for i in L :
+		n += i
+	return n/len(L)
+
 def MoyenneDistMoyenne(L) :
-	"""list[Number] -> Number
+	"""list[float] -> float
 	Renvoie la Moyenne des Distances à la Moyenne"""
 	M = Moyenne(L)
-	ret
+	return(MoyenneDistMoyenne([abs(M-i) for i in L]))
 
 def ListeSalaire(L) :
-	"""list[Individu] -> list[Number]
+	"""list[Individu] -> list[float]
 	Renvoie la liste des salaires de la liste d'individu"""
 	Ln = []
 	for i in L :
@@ -36,11 +41,14 @@ def ListeSalaire(L) :
 		Ln.append(i)
 	return Ln
 
-
 def ListeSalaireCroissant(L) :
 	"""List[Individu] -> List[int]
 	Renvoie la liste croissante des salaires de la liste d'individu"""
 	return RangementCroissant(ListeSalaire(L))
+
+def coeffgini(L) :
+
+	
 
 		
 	
