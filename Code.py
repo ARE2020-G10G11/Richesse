9910c31904a18b1d00dac6f11f7bf8922c4803bf
Paul Rieunier <61982963+PaulRieunier@users.noreply.github.com>
2020-04-27 15:24:34 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,6 +2,8 @@
 
 #type Individu = tuple[int,tuple[int,int],int,float]
 
+
+
 def MinEtPos(L) :
 	"""list[Number] -> list[float]
 	Renvoie le Minimum et sa position """
@@ -10,7 +12,7 @@ def MinEtPos(L) :
 			return [L[i],i]
 
 def RangementCroissant(L) :
-	"""list[int] -> list[int]
+	"""list[float] -> list[float]
 	Range la liste L dans l'odre croissant"""
 	if len(L) == 0 :
 		return []
@@ -35,16 +37,44 @@ def MoyenneDistMoyenne(L) :
 def ListeSalaire(L) :
 	"""list[Individu] -> list[float]
 	Renvoie la liste des salaires de la liste d'individu"""
-	Ln = []
+	Ls = []
 	for i in L :
-		_,_,_,_,k = i
-		Ln.append(i)
-	return Ln
+		_,_,_,_,S = i
+		Ln.append(S)
+	return Ls
 
 def ListeSalaireCroissant(L) :
-	"""List[Individu] -> List[int]
+	"""list[Individu] -> list[float]
 	Renvoie la liste croissante des salaires de la liste d'individu"""
 	return RangementCroissant(ListeSalaire(L))
+
+def ListeGeneration(L,n) :
+	"""list[list[Individu]]*int -> list[Individu]
+	Renvoie la liste des individu d'une Generation"""
+	return L[n]
+
+def ListeSalaireCroissantGeneration(L,n) :
+	"""list[list[Individu]]*int -> list[float]
+	Renvoie la liste croissante des salaires de la liste des individus d'une generation]"""
+	return ListeSalaireCroissant(ListeGeneration(L,n))
+
+def ArbreGenealogique(Ind,n,L) :
+	"""Individu*int*list[list[Individu]] -> list[Individu]
+	Renvoie la liste d'individu de l'arbre généalogique de Ind jusqu'à n générations en arrière"""
+	Id,P,G,S = Ind
+	P1,P2 = P
+	if G == 0 :
+		return []
+	else :
+		LP = []
+		for Parent in L[G-1] :
+			IdP,PP,_,_ = IndP
+			if IdP == P1 or IdP == P2 :
+				LP.append(IndP)
+				
+	return LP + ArbreGenealogique(LP[0],n-1,L) + ArbreGenealogique(LP[1],n,L)
+
+	
 
 def coeffgini(L) :
 
